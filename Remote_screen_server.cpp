45def3bb87fcbdba5ed21cfc705944ad3aa5e076
chengmei <2929773952@qq.com>
2024-07-01 22:54:40 +0800

    增加注释

--- Remote_screen_server.cpp
+++ Remote_screen_server.cpp
@@ -6,13 +6,20 @@
 #include "UI.h"
 int main()
 {
+    // 随机生成RSA公钥和私钥
     keygen(private_key,public_key);
 
+
+    // 定义IP地址和端口
     const wchar_t* Server_Address = L"127.0.0.1";
     int Server_Port = 5005;
+
+    // 初始化Server对象
     Server server(Server_Address, Server_Port);
+    // 创建线程执行Serve类成员函数start_server
     std::thread serverThread(&Server::start_server, &server);
-    serverThread.detach(); //线程在后台运行
+    // 线程在后台运行
+    serverThread.detach(); 
     UI_imgui::Render();
     while (!GetAsyncKeyState(VK_F1)) {}
     server.stop();
